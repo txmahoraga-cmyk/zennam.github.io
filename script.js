5a9c3f323886d598d498ef8ea0ce10075e001288
txmahoraga-cmyk <txmahoraga@gmail.com>
2026-03-22 20:58:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+body {
+  margin: 0;
+  background: #0d0d0d;
+  color: #f1f1f1;
+  font-family: Arial, sans-serif;
+  line-height: 1.6;
+}
+
+header {
+  text-align: center;
+  padding: 60px 20px;
+}
+
+h1 {
+  font-size: 38px;
+  letter-spacing: 2px;
+  margin-bottom: 10px;
+}
+
+p {
+  color: #aaa;
+  font-size: 14px;
+}
+
+.btn {
+  display: inline-block;
+  margin-top: 15px;
+  padding: 10px 22px;
+  background: white;
+  color: black;
+  text-decoration: none;
+  border-radius: 20px;
+  font-size: 14px;
+}
+
+section {
+  padding: 30px 20px;
+  text-align: center;
+}
+
+h2 {
+  margin-bottom: 15px;
+  font-size: 20px;
+  letter-spacing: 1px;
+}
+
+video {
+  width: 90%;
+  max-width: 400px;
+  margin: 10px;
+  border-radius: 12px;
+}
+
+.services p,
+.contact p {
+  max-width: 400px;
+  margin: 8px auto;
+}.logo {
+  width: 70px;
+  height: auto;
+  margin-bottom: 10px;
+}
